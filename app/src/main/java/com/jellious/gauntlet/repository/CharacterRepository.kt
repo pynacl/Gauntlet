a6bcb6b5468b61b68330fbcf2b44d198f803f18c
pynacl <jake@pynacl.com>
2021-03-07 18:25:11 -0600

    Forgot to actually implement the interface

--- app/src/main/java/com/jellious/gauntlet/repository/CharacterRepository.kt
+++ app/src/main/java/com/jellious/gauntlet/repository/CharacterRepository.kt
@@ -2,22 +2,22 @@ package com.jellious.gauntlet.repository
 
 import com.jellious.gauntlet.models.Character
 
-class CharacterRepository {
+class CharacterRepository : ICharacterRepository {
     private val characters = arrayListOf<Character>()
 
-    fun addCharacter(character: Character) {
+    override fun addCharacter(character: Character) {
         characters.add(character)
     }
 
-    fun getCharacter(name : String) : Character {
+    override fun getCharacter(name : String) : Character {
         return characters.first { it.name == name }
     }
 
-    fun getCharacters() : List<Character> {
+    override fun getCharacters() : List<Character> {
         return characters
     }
 
-    fun getCharacters(characterFitler : List<Character>) : List<Character> {
+    override fun getCharacters(characterFitler : List<Character>) : List<Character> {
         return characters.filter { characterFitler.contains(it) }
     }
 }
--- app/src/main/java/com/jellious/gauntlet/repository/ICharacterRepository.kt
+++ app/src/main/java/com/jellious/gauntlet/repository/ICharacterRepository.kt
@@ -5,4 +5,6 @@ import com.jellious.gauntlet.models.Character
 interface ICharacterRepository {
     fun addCharacter(character : Character)
     fun getCharacter(name : String) : Character
+    fun getCharacters() : List<Character>
+    fun getCharacters(characterFitler : List<Character>): List<Character>
 }
